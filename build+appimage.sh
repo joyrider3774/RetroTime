bc8dec4bb81070afc3ab7524128822fa1fadfa34
Willems Davy <joyrider3774@hotmail.com>
2023-05-14 17:17:47 +0200

    use sdl2-config from own compiled libs for appimage

--- build+appimage.sh
+++ build+appimage.sh
@@ -2,6 +2,8 @@
 
 echo "Building..."
 
+export PATH=${HOME}/dev/x86_64/sdl2/bin:${PATH}
+
 make clean
 make CPPFLAGS="-std=c++11 -I${HOME}/dev/x86_64/sdl2/include -I${HOME}/dev/x86_64/sdl2/include/SDL2" LDFLAGS="-L${HOME}/dev/x86_64/sdl2/lib -L/usr/lib"
 
