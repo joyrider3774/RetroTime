4409a262df1d5727542aeb018e93ccc295dcd45f
Willems Davy <joyrider3774@hotmail.com>
2023-05-14 18:54:10 +0200

    Remove libasound from appimage building it needs to use the system lib or can cause issues

--- appimage-x86_64.sh
+++ appimage-x86_64.sh
@@ -15,7 +15,15 @@ cp "$DATA/data/io.github.joyrider3774.retrotime.svg" "$PROJ.AppDir/$PROJ.svg"
 cp -R "$DATA" "$PROJ.AppDir/usr"
 cd "$PROJ.AppDir"
 cp "$SDL_LIB_DIR"/* usr/lib
-rm usr/lib/*.a
+rm -f usr/lib/*.a
+#remove some blacklisted libs as per https://github.com/AppImageCommunity/pkg2appimage/blob/master/excludelist
+#definatly need to remove libasound as it causes issues on systems not running pulse
+rm -f usr/lib/libasound*
+#if i removed these below even though they are blacklisted the game would not run on my "clean ubuntu 16.04 vm" so i still include them
+#rm -f usr/lib/libfreetype*
+#rm -f usr/lib/libharfbuzz*
+#rm -f usr/lib/libz*
+
 strip usr/bin/* usr/lib/*
 
 echo "[Desktop Entry]
