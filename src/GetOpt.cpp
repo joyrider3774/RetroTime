cd4d7b200011e98cfb413b4358e41badf9836630
Willems Davy <joyrider3774@hotmail.com>
2024-07-25 23:45:48 +0200

    fix warnings

--- src/GetOpt.cpp
+++ src/GetOpt.cpp
@@ -42,7 +42,7 @@ int     opterr = 1,             /* if error message should be printed */
 optind = 1,             /* index into parent argv vector */
 optopt,                 /* character checked for validity */
 optreset;               /* reset getopt */
-char* optarg;                /* argument associated with option */
+const char* optarg;                /* argument associated with option */
 
 #define BADCH   (int)'?'
 #define BADARG  (int)':'
@@ -54,7 +54,7 @@ char* optarg;                /* argument associated with option */
  */
 int getopt(int nargc, char* const nargv[], const char* ostr)
 {
-    static char* place = EMSG;              /* option letter processing */
+    static const char* place = EMSG;              /* option letter processing */
     const char* oli;                              /* option letter list index */
 
     if (optreset || !*place) {              /* update scanning pointer */
